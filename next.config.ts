c914cbeb6ab3174170e35c64d68b2d81d2830d50
Irham Tri Ahmadi <irhamtria@gmail.com>
2026-02-21 16:20:31 +0700

    feat: add config to optimize pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // Next.js runtime Image Optimization isn't supported in standard static export,
+  // so we tell the framework to offload images locally/statically if applicable.
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
