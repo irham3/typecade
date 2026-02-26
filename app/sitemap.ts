4092b50090297ed455d8cd46d1d118e913270632
Irham Tri Ahmadi <irhamtria@gmail.com>
2026-02-26 22:35:49 +0700

    fix: remove force static

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { LEARN_MODULES } from '@/features/learn/data/lessons';
 
+export const dynamic = "force-static";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     const baseUrl = 'https://typecade.pages.dev';
 
