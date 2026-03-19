e55f36431ce2f1c9da608428639962e30c57dab7
Hanif Ahmad <ahmadhanif759@gmail.com>
2026-03-19 21:01:24 +0700

    change /multiplayer path to /arena

--- app/race/client.tsx
+++ app/race/client.tsx
@@ -11,7 +11,7 @@ export function RaceClient() {
     return (
         <main className="flex-1 w-full max-w-5xl px-4 sm:px-6 flex flex-col items-center justify-start pb-12 sm:pb-20 relative pt-4 sm:pt-8">
             <div className="w-full flex justify-center mt-6">
-                <MultiplayerRace roomCode={roomCode} onLeave={() => router.push("/multiplayer")} />
+                <MultiplayerRace roomCode={roomCode} onLeave={() => router.push("/arena")} />
             </div>
         </main>
     );
